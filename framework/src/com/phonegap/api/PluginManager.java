1ba4648d759d7bf314ec64ca1513c483c86508c5
Joe Bowser <joe.bowser@nitobi.com>
2011-07-14 13:01:20 -0700

    Merging by hand, because there's been too many changes and I should have rebased

--- framework/src/com/phonegap/api/PluginManager.java
+++ framework/src/com/phonegap/api/PluginManager.java
@@ -19,6 +19,8 @@ import org.xmlpull.v1.XmlPullParserException;
 import android.content.Intent;
 import android.content.res.XmlResourceParser;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
 /**
@@ -34,6 +36,7 @@ public final class PluginManager {
 	
 	private final PhonegapActivity ctx;
 	private final WebView app;
+    private Plugin menuPlugin;
 	
     // Map URL schemes like foo: to plugins that want to handle those schemes
     // This would allow how all URLs are handled to be offloaded to a plugin
@@ -217,11 +220,15 @@ public final class PluginManager {
 				this.plugins.put(className, plugin);
 				plugin.setContext(this.ctx);
 				plugin.setView(this.app);
+				if(className.equals("com.phonegap.menu.AppMenu"))
+	            {
+	                this.menuPlugin = (Plugin) plugin;
+	            }
 				plugin.onResume(true);
 				return plugin;
 			}
     	} catch (Exception e) {
-    		  e.printStackTrace();
+		  e.printStackTrace();
     		  System.out.println("Error adding plugin "+className+".");
     	}
     	return null;
@@ -334,4 +341,50 @@ public final class PluginManager {
 		System.err.println("https://raw.github.com/phonegap/phonegap-android/master/framework/res/xml/plugins.xml");        
 		System.err.println("=====================================================================================");
 	}
+
+    
+    /**
+    * Menu options code
+    */
+    public boolean onMenuCreate(Menu menu) {
+        // This should never fail
+        if(this.menuPlugin != null)
+        {
+            return menuPlugin.buildMenu(menu);
+        }
+        else
+        {
+            return false;
+        }
+    }
+        
+    public boolean onMenuItemSelect(MenuItem item)
+    {
+        if(menuPlugin != null)
+        {
+            return menuPlugin.onItemSelect(item);
+        }
+        else
+            return true;
+    }
+
+
+    public boolean checkMenuRefresh() {
+        if(menuPlugin != null)
+        {
+          return menuPlugin.isMenuChanged();
+        }
+        return false;
+    }
+
+
+      public boolean onMenuItemSelected(MenuItem item) {
+        if(menuPlugin != null)
+        {
+          return menuPlugin.onMenuItemSelected(item);
+        }
+        else
+          return true;
+      }
+    
 }
